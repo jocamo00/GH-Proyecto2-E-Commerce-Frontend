88765920a7aaee25ea11bee955bbc89c5cb89014
Jose Carreres <jose.carreres.81@gmail.com>
2020-05-14 04:39:34 +0200

    Desestructurando el componente Product

--- src/components/NewProducts.jsx
+++ src/components/NewProducts.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react';
+import Product from '../components/Product';
 import axios from 'axios';
 
 
@@ -19,24 +20,8 @@ const NewProducts = () => {
                 <hr></hr>
 
                 {/* Productos */}
-                <div id="productos" className="contenedor-productos products row col-12">
-                    {products.map(product=>
-                        <div className="container-producto product col-sm-6 col-lg-4 col-xl-3" key={product._id}>
-                            <img class="d-block w-100" src={product.imgUrl} alt=""/>
-                            <div class="contenedor-info">
-                                <div class="contenedorMarca">
-                                    <p class="marca">{product.brand}</p>
-                                    <p class="modelo">{product.model}</p>
-                                </div>
-                                <div class="contenedorProcesador">
-                                    <p class="procesador">{product.processor}/&nbsp;</p>
-                                    <p class="ram">{product.memory}/&nbsp;</p>
-                                    <p class="hd">{product.hardDisk}</p>
-                                </div>
-                                <p class="precio">{product.price}<span>€</span></p> 
-                            </div>
-                        </div>
-                    )}
+                <div id="productos" className="contenedor-productos row col-12">
+                    {products.map(product=> <Product product={product}/>)}
                 </div>
                 {/* Fin productos */}
             </section>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 
-const Product = () => {
+const Product = ({product}) => {
     return ( 
-        <p>producto</p>
+        <div className="container-producto col-sm-6 col-lg-4 col-xl-3" key={product._id}>
+            <img class="d-block w-100" src={product.imgUrl} alt=""/>
+            <div class="contenedor-info">
+                <div class="contenedorMarca">
+                    <p class="marca">{product.brand}</p>
+                    <p class="modelo">{product.model}</p>
+                </div>
+                <div class="contenedorProcesador">
+                    <p class="procesador">{product.processor}/&nbsp;</p>
+                    <p class="ram">{product.memory}/&nbsp;</p>
+                    <p class="hd">{product.hardDisk}</p>
+                </div>
+                <p class="precio">{product.price}<span>€</span></p> 
+            </div>
+        </div>
      );
 }
  
